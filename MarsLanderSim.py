05fa46e37158d9a571874aec420794ae59dd52a1
33783 <33783@DESKTOP-SM40CUS>
2023-02-27 18:35:59 +0100

    Split Lander sim into separate file, and fixed an issue where i mixed up the angle and thrust

--- MarsLanderSim.py
+++ MarsLanderSim.py
@@ -1,26 +1,29 @@
-import math
 import pygame
-import time
-import random
 import numpy as np
-import copy
 import gym
-import math
-import matplotlib
-import matplotlib.pyplot as plt
-from collections import namedtuple, deque
-from itertools import count
-import pickle as pkl
-import os
+import time
 
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.nn.functional as F
+# Set up the Pygame window
+WORLD_WIDTH, WORLD_HEIGHT = 7000, 3000
+SCREEN_WIDTH, SCREEN_HEIGHT = 1400, 700
 
 
 def remap_scalar(x, old_min, old_max, new_min, new_max):
     return (x - old_min) / (old_max - old_min) * (new_max - new_min) + new_min
+
+
+def world_to_screen(point):
+    pointx = remap_scalar(point[0], 0, Lander.WORLD_WIDTH,
+                          0, MarsLanderEnv.SCREEN_WIDTH)
+    pointy = remap_scalar(point[1], 0, Lander.WORLD_HEIGHT,
+                          0, MarsLanderEnv.SCREEN_HEIGHT)
+    pointy = MarsLanderEnv.SCREEN_HEIGHT - pointy
+    return np.array([pointx, pointy])
+
+
+def scalar_world_to_screen(n):
+    return remap_scalar(n, 0, Lander.WORLD_HEIGHT,
+                        0, MarsLanderEnv.SCREEN_HEIGHT)
 
 
 class Lander:
@@ -79,7 +82,8 @@ class Lander:
                               np.array([[0, Lander.WORLD_HEIGHT],
                                         ground[0]])])
 
-        # disntance sensors spread out uniformly in circular pattern each one calculates the distance to the nearest surface (including map limits)
+        # disntance sensors spread out uniformly in circular pattern each one
+        # calculates the distance to the nearest surface (including map limits)
         self.num_distance_sensors = 8
         self.distance_sensors_angles = [remap_scalar(
             i, 0, self.num_distance_sensors, 0, 360) for i in range(self.num_distance_sensors)]
@@ -111,7 +115,7 @@ class Lander:
                 angleSum += Lander.angle_between(pointA, pointB, pointC)
             else:
                 angleSum -= Lander.angle_between(pointA, pointB, pointC)
-        return abs(abs(angleSum) - 2 * math.pi) < self.EPS
+        return abs(abs(angleSum) - 2 * np.pi) < self.EPS
 
     def step(self, thrust, angle, dt=1):
         self.angle = max(
@@ -220,7 +224,7 @@ class Lander:
                              world_to_screen(sensor_debug_pos +
                                              Lander.rotate_vec(self.state[4+i]*np.array([100, 0]), a)),
                              1)
-        #d = self.direction_to_landing_zone()
+        # d = self.direction_to_landing_zone()
         dir_debug_pos = np.array([500, 2800], dtype="float64")
         # Convert the polar coordinates to Cartesian coordinates
         p = np.array([self.state[2], self.state[3]])
@@ -349,20 +353,6 @@ class Lander:
             self.state[i] = val
 
 
-def world_to_screen(point):
-    pointx = remap_scalar(point[0], 0, Lander.WORLD_WIDTH,
-                          0, MarsLanderEnv.SCREEN_WIDTH)
-    pointy = remap_scalar(point[1], 0, Lander.WORLD_HEIGHT,
-                          0, MarsLanderEnv.SCREEN_HEIGHT)
-    pointy = MarsLanderEnv.SCREEN_HEIGHT - pointy
-    return np.array([pointx, pointy])
-
-
-def scalar_world_to_screen(n):
-    return remap_scalar(n, 0, Lander.WORLD_HEIGHT,
-                        0, MarsLanderEnv.SCREEN_HEIGHT)
-
-
 class MarsLanderEnv(gym.Env):
     metadata = {
         "render_modes": ["human", "rgb_array"],
@@ -405,11 +395,12 @@ class MarsLanderEnv(gym.Env):
         return self.lander.get_state(), info
 
     def step(self, action):
-        thrust = action // (6 + 6 + 1)
-        angle = (action % (6 + 6 + 1) - 6) * 15
-        action = [thrust, angle]
+        if isinstance(action, int):
+            thrust = action // (6 + 6 + 1)
+            angle = (action % (6 + 6 + 1) - 6) * 15
+            action = [thrust, angle]
 
-        self.lander.step(action[1], action[0])
+        self.lander.step(action[0], action[1])
 
         state = self.lander.get_state()
         assert len(state) == 4 + self.lander.num_distance_sensors
@@ -421,8 +412,8 @@ class MarsLanderEnv(gym.Env):
         reward -= abs(self.lander.velocity[0])
         reward -= abs(self.lander.velocity[1])/2
         # reset the reward if it's not the final step, we only reward at the end
-#        if not self.lander.crashed:
-#            reward = 0
+        if not self.lander.crashed:
+            reward = 0
         if self.lander.landed:
             reward = 3000
 
@@ -466,249 +457,51 @@ class MarsLanderEnv(gym.Env):
             self.isopen = False
 
 
-Transition = namedtuple('Transition',
-                        ('state', 'action', 'next_state', 'reward'))
-
-
-class ReplayMemory(object):
-
-    def __init__(self, capacity):
-        self.memory = deque([], maxlen=capacity)
-
-    def push(self, *args):
-        """Save a transition"""
-        self.memory.append(Transition(*args))
-
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
-
-
-class DQN(nn.Module):
-
-    def __init__(self, n_observations, n_actions):
-        super(DQN, self).__init__()
-        self.layer1 = nn.Linear(n_observations, 16)
-        self.layer2 = nn.Linear(16, 16)
-        self.layer3 = nn.Linear(16, n_actions)
-
-    # Called with either one element to determine next action, or a batch
-    # during optimization. Returns tensor([[left0exp,right0exp]...]).
-    def forward(self, x):
-        x = F.relu(self.layer1(x))
-        x = F.relu(self.layer2(x))
-        return self.layer3(x)
-
-
-# Set up the Pygame window
-WORLD_WIDTH, WORLD_HEIGHT = 7000, 3000
-SCREEN_WIDTH, SCREEN_HEIGHT = 1400, 700
-SURFACE_TOPOLOGY = [(0, 450), (300, 750), (1000, 450), (1500, 650), (1800, 850), (2000, 1950), (2200, 1850), (2400, 2000), (3100, 1800), (3150, 1550), (2500, 1600),
-                    (2200, 1550), (2100, 750), (2200, 150), (3200, 150), (3500, 450), (4000, 950), (4500, 1450), (5000, 1550), (5500, 1500), (6000, 950), (6999, 1750)]
-INIT_POSITION = np.array([6500, 2600])
-INIT_VELOCITY = np.array([0, 0])
-env = MarsLanderEnv(
-    INIT_POSITION, INIT_VELOCITY, SURFACE_TOPOLOGY, render_mode='human')
-# set up matplotlib
-is_ipython = 'inline' in matplotlib.get_backend()
-if is_ipython:
-    from IPython import display
-
-plt.ion()
-
-# if gpu is to be used
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# BATCH_SIZE is the number of transitions sampled from the replay buffer
-# GAMMA is the discount factor as mentioned in the previous section
-# EPS_START is the starting value of epsilon
-# EPS_END is the final value of epsilon
-# EPS_DECAY controls the rate of exponential decay of epsilon, higher means a slower decay
-# TAU is the update rate of the target network
-# LR is the learning rate of the AdamW optimizer
-BATCH_SIZE = 128
-GAMMA = 0.99
-EPS_START = 0.9
-EPS_END = 0.05
-EPS_DECAY = 50000
-TAU = 0.005
-LR = 1e-4
-
-# Get number of actions from gym action space
-n_actions = 5 * 13
-# Get the number of state observations
-state, info = env.reset()
-n_observations = len(state)
-
-policy_net = DQN(n_observations, n_actions).to(device)
-target_net = DQN(n_observations, n_actions).to(device)
-target_net.load_state_dict(policy_net.state_dict())
-
-optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
-memory = ReplayMemory(10000)
-
-episode_durations = []
-
-steps_done = 0
-
-
-def select_action(state):
-    global steps_done
-    sample = random.random()
-    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
-        math.exp(-1. * steps_done / EPS_DECAY)
-    steps_done += 1
-    if sample > eps_threshold:
-        with torch.no_grad():
-            # t.max(1) will return the largest column value of each row.
-            # second column on max result is index of where max element was
-            # found, so we pick action with the larger expected reward.
-            return policy_net(state).max(1)[1].view(1, 1)
-    else:
-        t, a = env.action_space.sample()
-        encoded_action = t * (6 + 6 + 1) + a + 6
-        return torch.tensor(np.array([[encoded_action]]), device=device, dtype=torch.long)
-
-
-def plot_durations(show_result=False):
-    plt.figure(1)
-    durations_t = torch.tensor(episode_durations, dtype=torch.float)
-    if show_result:
-        plt.title('Result')
-    else:
-        plt.clf()
-        plt.title('Training...')
-    plt.xlabel('Episode')
-    plt.ylabel('Duration')
-    plt.plot(durations_t.numpy())
-    # Take 100 episode averages and plot them too
-    if len(durations_t) >= 100:
-        means = durations_t.unfold(0, 100, 1).mean(1).view(-1)
-        means = torch.cat((torch.zeros(99), means))
-        plt.plot(means.numpy())
-
-    plt.pause(0.001)  # pause a bit so that plots are updated
-    if is_ipython:
-        if not show_result:
-            display.display(plt.gcf())
-            display.clear_output(wait=True)
-        else:
-            display.display(plt.gcf())
-
-
-def optimize_model():
-    if len(memory) < BATCH_SIZE:
-        return
-    transitions = memory.sample(BATCH_SIZE)
-    # Transpose the batch (see https://stackoverflow.com/a/19343/3343043 for
-    # detailed explanation). This converts batch-array of Transitions
-    # to Transition of batch-arrays.
-    batch = Transition(*zip(*transitions))
-
-    # Compute a mask of non-final states and concatenate the batch elements
-    # (a final state would've been the one after which simulation ended)
-    non_final_mask = torch.tensor(tuple(map(lambda s: s is not None,
-                                            batch.next_state)), device=device, dtype=torch.bool)
-    non_final_next_states = torch.cat([s for s in batch.next_state
-                                       if s is not None])
-    state_batch = torch.cat(batch.state)
-    action_batch = torch.cat(batch.action)
-    reward_batch = torch.cat(batch.reward)
-
-    # Compute Q(s_t, a) - the model computes Q(s_t), then we select the
-    # columns of actions taken. These are the actions which would've been taken
-    # for each batch state according to policy_net
-    state_action_values = policy_net(state_batch).gather(1, action_batch)
-
-    # Compute V(s_{t+1}) for all next states.
-    # Expected values of actions for non_final_next_states are computed based
-    # on the "older" target_net; selecting their best reward with max(1)[0].
-    # This is merged based on the mask, such that we'll have either the expected
-    # state value or 0 in case the state was final.
-    next_state_values = torch.zeros(BATCH_SIZE, device=device)
-    with torch.no_grad():
-        next_state_values[non_final_mask] = target_net(
-            non_final_next_states).max(1)[0]
-    # Compute the expected Q values
-    expected_state_action_values = (next_state_values * GAMMA) + reward_batch
-
-    # Compute Huber loss
-    criterion = nn.SmoothL1Loss()
-    loss = criterion(state_action_values,
-                     expected_state_action_values.unsqueeze(1))
-
-    # Optimize the model
-    optimizer.zero_grad()
-    loss.backward()
-    # In-place gradient clipping
-    torch.nn.utils.clip_grad_value_(policy_net.parameters(), 100)
-    optimizer.step()
-
-
-if torch.cuda.is_available():
-    num_episodes = 6000
-else:
-    num_episodes = 50
-
-for i_episode in range(num_episodes):
-    # Initialize the environment and get it's state
-    state, info = env.reset()
-    state = torch.tensor(state, dtype=torch.float32,
-                         device=device).unsqueeze(0)
-    if i_episode % 1000 == 0:
-        with open(f"models/memory_buffer_ep{i_episode:}_{steps_done}.pkl", 'wb') as f:
-            pkl.dump(memory, f)
-        with open(f"models/episode_rewards_ep{i_episode:}_{steps_done}.pkl", "wb") as f:
-            pkl.dump(episode_durations, f)
-
-        torch.save(policy_net.state_dict(),
-                   f"models/policy_net_ep{i_episode:}_{steps_done}.pth")
-        torch.save(target_net.state_dict(),
-                   f"models/target_net_ep{i_episode:}_{steps_done}.pth")
-    if i_episode % 1 == 0:
-        INIT_POSITION[0] = random.randint(200, WORLD_WIDTH-200)
-    #steps_done = 0
-    for t in count():
-        # Debug message
-        action = select_action(state)
-        observation, reward, terminated, _ = env.step(action.item())
-        reward = torch.tensor([reward], device=device)
-        done = terminated
-        if t % 50 == 0 or done:
-            print(
-                f"Step {t} of episode {i_episode} {env.lander.position} {reward.item()}")
-        if terminated:
-            next_state = None
-        else:
-            next_state = torch.tensor(
-                observation, dtype=torch.float32, device=device).unsqueeze(0)
-
-        # Store the transition in memory
-        memory.push(state, action, next_state, reward)
-
-        # Move to the next state
-        state = next_state
-
-        # Perform one step of the optimization (on the policy network)
-        optimize_model()
-
-        # Soft update of the target network's weights
-        # θ′ ← τ θ + (1 −τ )θ′
-        target_net_state_dict = target_net.state_dict()
-        policy_net_state_dict = policy_net.state_dict()
-        for key in policy_net_state_dict:
-            target_net_state_dict[key] = policy_net_state_dict[key] * \
-                TAU + target_net_state_dict[key]*(1-TAU)
-        target_net.load_state_dict(target_net_state_dict)
-
+def demo_manual_lander():
+    print("Running manual controlled lander")
+    SURFACE_TOPOLOGY = [(0, 450), (300, 750), (1000, 450), (1500, 650), (1800, 850), (2000, 1950), (2200, 1850), (2400, 2000), (3100, 1800), (3150, 1550), (2500, 1600),
+                        (2200, 1550), (2100, 750), (2200, 150), (3200, 150), (3500, 450), (4000, 950), (4500, 1450), (5000, 1550), (5500, 1500), (6000, 950), (6999, 1750)]
+    INIT_POSITION = np.array([6500, 2600])
+    INIT_VELOCITY = np.array([0, 0])
+    env = MarsLanderEnv(
+        INIT_POSITION, INIT_VELOCITY, SURFACE_TOPOLOGY, render_mode='human')
+    desired_thrust, desired_angle = 0, 0
+    s, info = env.reset()
+    i = 0
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pass
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    desired_angle = 10
+                elif event.key == pygame.K_RIGHT:
+                    desired_angle = -10
+                elif event.key == pygame.K_UP:
+                    desired_thrust = 4
+                elif event.key == pygame.K_DOWN:
+                    desired_thrust = 0
+                elif event.key == pygame.K_F5:
+                    # Reset the lander and surface
+                    env.reset()
+            elif event.type == pygame.KEYUP:
+                # Reset the desired angle and thrust when the keys are released
+                desired_angle = 0
+                desired_thrust = 0
+        time.sleep(.05)
+        action = [desired_thrust, desired_angle]
+        observation, reward, done, info = env.step(action)
+        i += 1
+        print(i, env.lander.position.round(),
+              env.lander.angle, env.lander.thrust,
+              env.lander.velocity.round(), env.lander.landed)
+        # print(observation, reward, done, info)
         if done:
-            episode_durations.append(reward)
-            plot_durations()
+            print("Landed status:", env.lander.landed)
             break
 
-print('Complete')
-plot_durations(show_result=True)
-plt.ioff()
-plt.show()
+    env.close()
+
+
+if __name__ == "__main__":
+    demo_manual_lander()
